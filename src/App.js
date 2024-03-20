a1bd0aba868af789e7dff75f6bc93dba9b4d1c89
CezarLuca <71207294+CezarLuca@users.noreply.github.com>
2024-03-20 13:59:44 +0100

    Setting up the converter and the css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,63 @@
+import { useState } from "react";
+
 function App() {
-    return <div className="App">Hello World!</div>;
+    const [amountToConvert, setAmountToConvert] = useState(0);
+    const [fromCurrency, setFromCurrency] = useState("");
+    const [toCurrency, setToCurrency] = useState("");
+    const [conversionResult, setConversionResult] = useState(0);
+
+    return (
+        <div className="App">
+            <h1>Currency Converter</h1>
+            <p>
+                <span>
+                    <label>Amount</label>
+                    <input type="text" />
+                </span>
+                <span>
+                    <label>From</label>
+                    <select>
+                        <option value={"USD"}>USD</option>
+                        <option value={"EUR"}>EUR</option>
+                        <option value={"CNY"}>CNY</option>
+                        <option value={"JPY"}>JPY</option>
+                        <option value={"KRW"}>KRW</option>
+                        <option value={"INR"}>INR</option>
+                        <option value={"GBP"}>GBP</option>
+                        <option value={"CHF"}>CHF</option>
+                        <option value={"AED"}>AED</option>
+                        <option value={"RON"}>RON</option>
+                        <option value={"PLN"}>PLN</option>
+                    </select>
+                </span>
+                <span>
+                    <label>To</label>
+                    <select>
+                        <option value={"USD"}>USD</option>
+                        <option value={"EUR"}>EUR</option>
+                        <option value={"CNY"}>CNY</option>
+                        <option value={"JPY"}>JPY</option>
+                        <option value={"KRW"}>KRW</option>
+                        <option value={"INR"}>INR</option>
+                        <option value={"GBP"}>GBP</option>
+                        <option value={"CHF"}>CHF</option>
+                        <option value={"AED"}>AED</option>
+                        <option value={"RON"}>RON</option>
+                        <option value={"PLN"}>PLN</option>
+                    </select>
+                </span>
+            </p>
+            <p>
+                <span>
+                    <label>Conversion Result</label>
+                    <input type="text" readOnly />
+                </span>
+            </p>
+            <p className="ConvertBtn">
+                <button>Convert</button>
+            </p>
+        </div>
+    );
 }
 
 export default App;
